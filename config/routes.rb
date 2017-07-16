8fcb38d4ee6c157021127ec87db327d87cc827af
Paulo Cesar <pcdsjr@gmail.com>
2017-07-16 16:41:27 -0500

    chore: add controllers and mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'members/create'
+
+  get 'members/update'
+
+  get 'members/destroy'
+
+  get 'campaings/show'
+
+  get 'campaings/index'
+
+  get 'campaings/create'
+
+  get 'campaings/update'
+
+  get 'campaings/destroy'
+
+  get 'campaings/raffle'
+
+  get 'pages/home'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   mount Sidekiq::Web => '/sidekiq'
 end
