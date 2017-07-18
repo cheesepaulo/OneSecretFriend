751459bff78280290b845ca8805546e5b7388316
Paulo Cesar <pcdsjr@gmail.com>
2017-07-18 11:16:56 -0500

    chore: set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,14 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  get 'members/create'
-
-  get 'members/update'
-
-  get 'members/destroy'
-
-  get 'campaings/show'
-
-  get 'campaings/index'
-
-  get 'campaings/create'
-
-  get 'campaings/update'
-
-  get 'campaings/destroy'
-
-  get 'campaings/raffle'
-
-  get 'pages/home'
-
   devise_for :users, :controllers => { registrations: 'registrations' }
   mount Sidekiq::Web => '/sidekiq'
+
+  root to: 'pages#home'
+  resources :campaigns, except: [:new] do
+    post 'raffle', on: :member
+    # post 'raffle', on: :collection
+  end
+  get 'members/:token/opened', to: 'members#opened'
+  resources :members, only: [:create, :destroy, :update]
 end
